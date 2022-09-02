9b12f88ab89349ef74edf328a91705797d1949ad
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2022-09-01 23:08:41 -0400

    added error handling, polished details

--- frontend/src/pages/EmbeddedPage.tsx
+++ frontend/src/pages/EmbeddedPage.tsx
@@ -7,6 +7,7 @@ import EmbeddedData from "models/EmbeddedData";
 
 import useAsync from "hooks/useAsync";
 import { getEmbeddedData } from "services/Embedded";
+import { AxiosError } from "axios";
 
 const EmbeddedPage = () => {
   const [embeddedInput, setEmbeddedInput] = useState<string>("");
@@ -16,15 +17,16 @@ const EmbeddedPage = () => {
     status,
     error,
     value: embeddedData,
-  } = useAsync<EmbeddedData[]>(async () => {
-    return await getEmbeddedData(embeddedInput);
-  }, false);
+  } = useAsync<EmbeddedData[], AxiosError<{ message: string }>>(
+    () => getEmbeddedData(embeddedInput),
+    false
+  );
 
   let results = null;
   if (status === "pending") {
     results = <LinearProgress></LinearProgress>;
   } else if (status === "error") {
-    results = <>{error}</>;
+    results = <>{error?.response?.data.message}</>;
   } else if (status === "success") {
     results = (
       <Stack spacing={2}>
--- frontend/src/services/Embedded.ts
+++ frontend/src/services/Embedded.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 import EmbeddedData from "models/EmbeddedData";
 
-export const getEmbeddedData = async (value: string) => {
+export const getEmbeddedData = async (text: string) => {
   const res = await axios.post<EmbeddedData[]>(
     "https://882j6754y6.execute-api.us-east-1.amazonaws.com/",
-    {
-      text: value,
-    }
+    { text }
   );
-
   return res.data;
 };
